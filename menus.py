158e5d2528d01ad081c1606775da9ea0d40f3ad2
solongtony <solongtony@gmail.com>
2018-05-05 17:07:49 -0400

    dry up character_screen

--- menus.py
+++ menus.py
@@ -66,25 +66,31 @@ def level_up_menu(con, header, player, menu_width, screen_width, screen_height):
     menu(con, header, options, menu_width, screen_width, screen_height)
 
 
+def make_window_printer(window, width, height, background, alignment):
+    def window_printer(x, y, message):
+        libtcod.console_print_rect_ex(
+            window,
+            x, y,
+            width, height,
+            background,
+            alignment,
+            message)
+    return window_printer
+
 def character_screen(player, character_screen_width, character_screen_height, screen_width, screen_height):
     window = libtcod.console_new(character_screen_width, character_screen_height)
 
     libtcod.console_set_default_foreground(window, libtcod.white)
+    print = make_window_printer(window, character_screen_width, character_screen_height, libtcod.BKGND_NONE, libtcod.LEFT)
 
-    libtcod.console_print_rect_ex(window, 0, 1, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
-                                  libtcod.LEFT, 'Character Information')
-    libtcod.console_print_rect_ex(window, 0, 2, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
-                                  libtcod.LEFT, 'Level: {0}'.format(player.level.current_level))
-    libtcod.console_print_rect_ex(window, 0, 3, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
-                                  libtcod.LEFT, 'Experience: {0}'.format(player.level.current_xp))
-    libtcod.console_print_rect_ex(window, 0, 4, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
-                                  libtcod.LEFT, 'Experience to Level: {0}'.format(player.level.experience_to_next_level))
-    libtcod.console_print_rect_ex(window, 0, 6, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
-                                  libtcod.LEFT, 'Maximum HP: {0}'.format(player.fighter.max_hp))
-    libtcod.console_print_rect_ex(window, 0, 7, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
-                                  libtcod.LEFT, 'Attack: {0}'.format(player.fighter.power))
-    libtcod.console_print_rect_ex(window, 0, 8, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
-                                  libtcod.LEFT, 'Defense: {0}'.format(player.fighter.defense))
+    print(0, 1, 'Character Information')
+    print(0, 2, 'Level: {0}'.format(player.level.current_level))
+    print(0, 3, 'Experience: {0}'.format(player.level.current_xp))
+    print(0, 4, 'Experience to Level: {0}'.format(player.level.experience_to_next_level))
+
+    print(0, 6, 'Maximum HP: {0}'.format(player.fighter.max_hp))
+    print(0, 7, 'Attack: {0}'.format(player.fighter.power))
+    print(0, 8, 'Defense: {0}'.format(player.fighter.defense))
 
     x = screen_width // 2 - character_screen_width // 2
     y = screen_height // 2 - character_screen_height // 2
