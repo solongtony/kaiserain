9b89d736ae457e3c20105fdb0ab75f64bc43c909
solongtony <solongtony@gmail.com>
2018-05-06 22:34:52 -0400

    code cleanup in GameMap

--- src/map_objects/game_map.py
+++ src/map_objects/game_map.py
@@ -16,37 +16,25 @@ from map_objects.tile import Tile
 from random_utils import from_dungeon_level, random_choice_from_dict
 from render_functions import RenderOrder
 
+STAIRS_DOWN_CHARACTER = '>'
 
 class GameMap:
     def __init__(self, width, height, dungeon_level=1):
         self.width = width
         self.height = height
         self.tiles = self.initialize_tiles()
-
         self.dungeon_level = dungeon_level
 
     def initialize_tiles(self):
-        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
+        return [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
-        return tiles
-
+    # TODO: make most of these class fields, and avoid passing them around as params.
     def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities):
         rooms = []
         num_rooms = 0
 
-        center_of_last_room_x = None
-        center_of_last_room_y = None
-
         for r in range(max_rooms):
-            # random width and height
-            w = randint(room_min_size, room_max_size)
-            h = randint(room_min_size, room_max_size)
-            # random position without going out of the boundaries of the map
-            x = randint(0, map_width - w - 1)
-            y = randint(0, map_height - h - 1)
-
-            # "Rect" class makes rectangles easier to work with
-            new_room = Rect(x, y, w, h)
+            new_room = GameMap.create_room(room_min_size, room_max_size, map_width, map_height)
 
             # run through the other rooms and see if they intersect with this one
             for other_room in rooms:
@@ -55,17 +43,12 @@ class GameMap:
             else:
                 # this means there are no intersections, so this room is valid
 
-                # "paint" it to the map's tiles
-                self.create_room(new_room)
-
-                # center coordinates of new room, will be useful later
+                self.unblock_room_tiles(new_room)
                 (new_x, new_y) = new_room.center()
 
-                center_of_last_room_x = new_x
-                center_of_last_room_y = new_y
-
                 if num_rooms == 0:
-                    # this is the first room, where the player starts at
+                    # this is the first room, where the player starts
+                    # TODO: stairs up would go here.
                     player.x = new_x
                     player.y = new_y
                 else:
@@ -78,12 +61,12 @@ class GameMap:
                     # flip a coin (random number that is either 0 or 1)
                     if randint(0, 1) == 1:
                         # first move horizontally, then vertically
-                        self.create_h_tunnel(prev_x, new_x, prev_y)
-                        self.create_v_tunnel(prev_y, new_y, new_x)
+                        self.unblock_h_tunnel(prev_x, new_x, prev_y)
+                        self.unblock_v_tunnel(prev_y, new_y, new_x)
                     else:
                         # first move vertically, then horizontally
-                        self.create_v_tunnel(prev_y, new_y, prev_x)
-                        self.create_h_tunnel(prev_x, new_x, new_y)
+                        self.unblock_v_tunnel(prev_y, new_y, prev_x)
+                        self.unblock_h_tunnel(prev_x, new_x, new_y)
 
                 self.place_entities(new_room, entities)
 
@@ -92,49 +75,53 @@ class GameMap:
                 num_rooms += 1
 
         stairs_component = Stairs(self.dungeon_level + 1)
-        down_stairs = Entity(center_of_last_room_x, center_of_last_room_y, '>', libtcod.white, 'Stairs',
-                             render_order=RenderOrder.STAIRS, stairs=stairs_component)
+        down_stairs = Entity(
+            new_x, new_y, STAIRS_DOWN_CHARACTER, libtcod.white, 'Stairs',
+            render_order=RenderOrder.STAIRS, stairs=stairs_component)
         entities.append(down_stairs)
 
-    def create_room(self, room):
+    @staticmethod
+    def create_room(room_min_size, room_max_size, map_width, map_height):
+        # random width and height
+        w = randint(room_min_size, room_max_size)
+        h = randint(room_min_size, room_max_size)
+        # random position without going out of the boundaries of the map
+        x = randint(0, map_width - w - 1)
+        y = randint(0, map_height - h - 1)
+
+        # "Rect" class makes rectangles easier to work with
+        return Rect(x, y, w, h)
+
+    def unblock_room_tiles(self, room):
         # go through the tiles in the rectangle and make them passable
         for x in range(room.x1 + 1, room.x2):
             for y in range(room.y1 + 1, room.y2):
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
 
-    def create_h_tunnel(self, x1, x2, y):
+    def unblock_h_tunnel(self, x1, x2, y):
         for x in range(min(x1, x2), max(x1, x2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-    def create_v_tunnel(self, y1, y2, x):
+    def unblock_v_tunnel(self, y1, y2, x):
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
     def place_entities(self, room, entities):
+        self.place_monsters(room, entities)
+        self.place_items(room, entities)
+
+    def place_monsters(self, room, entities):
         max_monsters_per_room = from_dungeon_level([[2, 1], [3, 4], [5, 6]], self.dungeon_level)
-        max_items_per_room = from_dungeon_level([[1, 1], [2, 4]], self.dungeon_level)
 
         # Get a random number of monsters
         number_of_monsters = randint(0, max_monsters_per_room)
 
-        # Get a random number of items
-        number_of_items = randint(0, max_items_per_room)
-
         monster_chances = {
             'orc': 80,
             'troll': from_dungeon_level([[15, 3], [30, 5], [60, 7]], self.dungeon_level)
-        }
-
-        item_chances = {
-            'healing_potion': 35,
-            'sword': from_dungeon_level([[5, 4]], self.dungeon_level),
-            'shield': from_dungeon_level([[15, 8]], self.dungeon_level),
-            'lightning_scroll': from_dungeon_level([[25, 4]], self.dungeon_level),
-            'fireball_scroll': from_dungeon_level([[25, 6]], self.dungeon_level),
-            'confusion_scroll': from_dungeon_level([[10, 2]], self.dungeon_level)
         }
 
         for i in range(number_of_monsters):
@@ -152,6 +139,21 @@ class GameMap:
                     monster = CreatureTypes.make_creature_entity(x, y, CreatureTypes.TROLL)
                 entities.append(monster)
 
+    def place_items(self, room, entities):
+        max_items_per_room = from_dungeon_level([[1, 1], [2, 4]], self.dungeon_level)
+
+        # Get a random number of items
+        number_of_items = randint(0, max_items_per_room)
+
+        item_chances = {
+            'healing_potion': 35,
+            'sword': from_dungeon_level([[5, 4]], self.dungeon_level),
+            'shield': from_dungeon_level([[15, 8]], self.dungeon_level),
+            'lightning_scroll': from_dungeon_level([[25, 4]], self.dungeon_level),
+            'fireball_scroll': from_dungeon_level([[25, 6]], self.dungeon_level),
+            'confusion_scroll': from_dungeon_level([[10, 2]], self.dungeon_level)
+        }
+
         for i in range(number_of_items):
             x = randint(room.x1 + 1, room.x2 - 1)
             y = randint(room.y1 + 1, room.y2 - 1)
@@ -159,6 +161,7 @@ class GameMap:
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
                 item_choice = random_choice_from_dict(item_chances)
 
+                # TODO: make ItemTypes like the existing CreatureTypes to clean up this code.
                 if item_choice == 'healing_potion':
                     item_component = Item(use_function=heal, amount=40)
                     item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM,
@@ -188,10 +191,7 @@ class GameMap:
                 entities.append(item)
 
     def is_blocked(self, x, y):
-        if self.tiles[x][y].blocked:
-            return True
-
-        return False
+        return self.tiles[x][y].blocked
 
     def next_floor(self, player, message_log, constants):
         self.dungeon_level += 1
